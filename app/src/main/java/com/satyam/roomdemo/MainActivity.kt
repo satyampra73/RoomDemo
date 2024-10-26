9c8f9ffe8585fb1b68ebbdb2baed5d78033cce34
Satyam Prajapati <satyam7355pra@gmail.com>
2024-10-26 18:21:12 +0530

    working on View

--- app/src/main/java/com/satyam/roomdemo/MainActivity.kt
+++ app/src/main/java/com/satyam/roomdemo/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.satyam.roomdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
 
     }
---
+++ app/src/main/java/com/satyam/roomdemo/room/SubscriberRepository.kt
@@ -0,0 +1,23 @@
+package com.satyam.roomdemo.room
+
+class SubscriberRepository (private val dao : SubscriberDAO){
+    val subscriber = dao.getAllSubscribers()
+
+    suspend fun insert (subscriber: Subscriber){
+        dao.insertSubscriber(subscriber)
+    }
+
+    suspend fun update (subscriber: Subscriber){
+        dao.updateSubscriber(subscriber)
+    }
+
+    suspend fun delete(subscriber : Subscriber){
+        dao.deleteSubscriber(subscriber)
+    }
+
+    suspend fun deleteAll(subscriber: Subscriber){
+        dao.deleteAll()
+    }
+
+
+}
