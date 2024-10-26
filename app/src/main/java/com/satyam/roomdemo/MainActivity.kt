97b54015a413643da5a68e5194550eed9743ee86
Satyam Prajapati <satyam7355pra@gmail.com>
2024-10-26 12:28:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/satyam/roomdemo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.satyam.roomdemo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.satyam.roomdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
