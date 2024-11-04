d057d48bc3fcd4b3cb56ef52088392cae697f34e
Satyam Prajapati <satyam7355pra@gmail.com>
2024-11-04 13:48:44 +0530

    Schemas Enabled and Room Migrations done (Coumn Name Changes Done)

--- app/src/main/java/com/satyam/roomdemo/room/Subscriber.kt
+++ app/src/main/java/com/satyam/roomdemo/room/Subscriber.kt
@@ -7,9 +7,9 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "subscriber_data_table")
 data class Subscriber (
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "subscriber_id")
+    @ColumnInfo(name = "subs_id")
     val id : Int,
-    @ColumnInfo(name = "subscriber_name")
+    @ColumnInfo(name = "subs_name")
     var name : String,
     @ColumnInfo(name = "subscriber_email")
     var email : String
--- app/src/main/java/com/satyam/roomdemo/room/SubscriberDatabase.kt
+++ app/src/main/java/com/satyam/roomdemo/room/SubscriberDatabase.kt
@@ -4,12 +4,24 @@ import android.content.Context
 import android.content.IntentFilter
 import android.provider.CalendarContract.Instances
 import androidx.core.os.persistableBundleOf
+import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
+import androidx.room.RenameTable
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 
-@Database(entities = [Subscriber::class], version = 1, exportSchema = false)
+@Database(entities = [Subscriber::class], version = 3, exportSchema = true,
+    autoMigrations = [
+        AutoMigration(from = 1,to = 2, spec = SubscriberDatabase.Migration1to2::class),
+        AutoMigration(from = 2,to = 3, spec = SubscriberDatabase.Migration2to3::class),
+    ])
 abstract class SubscriberDatabase : RoomDatabase() {
+    @RenameColumn(tableName = "subscriber_data_table" , fromColumnName = "subscriber_id", toColumnName = "subs_id")
+    class Migration1to2 : AutoMigrationSpec
+    @RenameColumn(tableName = "subscriber_data_table" , fromColumnName = "subscriber_name", toColumnName = "subs_name")
+    class Migration2to3 : AutoMigrationSpec
 
     abstract val subscriberDAO: SubscriberDAO
     companion object{
